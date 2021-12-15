da427ebb3fdcdc04c1edb7747b956a84e1104c22
delta114514 <gey3933@gmail.com>
2021-12-16 01:08:22 +0900

    update notify

--- controller.py
+++ controller.py
@@ -2,10 +2,32 @@ import asyncio
 import tkinter as tk
 from typing import Union
 from bleak import discover, BleakClient
+from bleak.exc import BleakError
 from datetime import datetime, timedelta
 from plyer import notification
-
+from functools import wraps
+from warnings import warn
 from utils import *
+
+
+def ble_error_catch(func):
+    if asyncio.iscoroutinefunction(func):
+        @wraps(func)
+        async def async_inner(self, *args, **kwargs):
+            try:
+                return await func(self, *args, **kwargs)
+            except (RuntimeError, BleakError):
+                warn(f"'{func.__name__}' was failed because of Bluetooth error.")
+        return async_inner
+
+    else:
+        @wraps(func)
+        def inner(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except (RuntimeError, BleakError):
+                warn(f"'{func.__name__}' was failed because of Bluetooth error.")
+        return inner
 
 
 class Controller:
@@ -51,10 +73,10 @@ class Controller:
         await asyncio.gather(self.set_schedule(), self.initial_fetch_values(True))
 
     def notify(self):
-        if self.last_notify is not None and self.last_notify > datetime.now() - timedelta(seconds=self.notify_interval):
-            return
-
+        last = self.last_notify
         self.last_notify = datetime.now()
+        if last is not None and last > datetime.now() - timedelta(seconds=self.notify_interval):
+            return
 
         if self.temperature_scale == TemperatureScale.Celsius:
             temp = '{}°C'.format(self.setting_temperature)
@@ -64,22 +86,27 @@ class Controller:
                             message='Your drink is waiting for you to drink!. It\'s nice and warm {}!'.format(temp),
                             app_name='Ember Mug Controller')
 
+    @ble_error_catch
     async def fetch_battery_state(self):
         value = await self.client.read_gatt_char(Request.Battery.as_uuid)
         self.battery = parse_battery(value)
 
+    @ble_error_catch
     async def fetch_temperature(self):
         value = await self.client.read_gatt_char(Request.Temperature.as_uuid)
         self.temperature = decode_temperature(value)
 
+    @ble_error_catch
     async def fetch_setting_temperature(self):
         value = await self.client.read_gatt_char(Request.SettingTemperature.as_uuid)
         self.setting_temperature = decode_temperature(value)
 
+    @ble_error_catch
     async def set_setting_temperature(self, value: float):
         await self.client.write_gatt_char(Request.SettingTemperature.as_uuid, encode_temperature(value))
         self.setting_temperature = value
 
+    @ble_error_catch
     async def fetch_state(self):
         value = await self.client.read_gatt_char(Request.State.as_uuid)
         state = State(value[0])
@@ -89,22 +116,27 @@ class Controller:
             self.notify()
         self.state = state
 
+    @ble_error_catch
     async def fetch_color(self):
         value = await self.client.read_gatt_char(Request.LightColor.as_uuid)
         self.color = parse_color(value)
 
+    @ble_error_catch
     async def set_color(self, color: Color):
         await self.client.write_gatt_char(Request.LightColor.as_uuid, color.as_bytearray)
         self.color = color
 
+    @ble_error_catch
     async def fetch_temperature_scale(self):
         value = await self.client.read_gatt_char(Request.TemperatureScale.as_uuid)
         self.temperature_scale = TemperatureScale(value[0])
 
+    @ble_error_catch
     async def set_temperature_scale(self, scale: TemperatureScale):
         await self.client.write_gatt_char(Request.TemperatureScale.as_uuid, scale.as_bytearray)
         self.temperature_scale = scale
 
+    @ble_error_catch
     async def set_schedule(self):
         while True:
             if not self.running:
@@ -137,6 +169,7 @@ class Controller:
         await self.client.disconnect()
 
     def notify_callback(self):
+        @ble_error_catch
         async def callback(_: int, data: bytearray) -> None:
             if not self.running:
                 return await self.client.stop_notify(Request.Notification.as_uuid)
